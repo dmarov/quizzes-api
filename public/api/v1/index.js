8e0110e0f3ca4f2a98dff5d584e47d1b17f6a8ed
Dmitry Marov <d.marov94@gmail.com>
2018-09-05 17:04:44 +0300

    update

--- public/api/v1/index.js
+++ public/api/v1/index.js
@@ -6,7 +6,6 @@ const bodyParser = require('koa-bodyparser');
 const path = require('path');
 const fs = require('fs');
 const massive = require('massive');
-const di = require('./di');
 
 const jwtParser = require('./middleware/jwt-parser')
 const Routes = require('./routes');
@@ -20,8 +19,6 @@ module.exports = async _ => {
         password: "NRuWlO0uLsG75ySUSE3KDuF7or0M1WLwEkTcXk8q",
         database: "quizzes_api_v1",
     });
-
-    di.append('db', db);
 
     const DOC_PATH = path.resolve(__dirname, '../../../doc/api/v1/openapi.html');
 
@@ -39,7 +36,7 @@ module.exports = async _ => {
     let app = new Koa();
 
     app.use(async (ctx, next) => {
-        ctx.db = di.get('db');
+        ctx.db = db;
         await next();
     });
 
