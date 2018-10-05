2a2e6cdfd9d09a24a5e36be3922fc859e0067fc6
Dmitry Marov <d.marov94@gmail.com>
2018-10-05 12:50:05 +0300

    update

--- tests/response-get-0.sh
+++ tests/response-get-0.sh
@@ -1,4 +1,4 @@
 #!/usr/local/bin/zsh
 
-curl -X GET -i 'http://quizzes.websm.io/users/test/quizzes/2/responses/2' \
+curl -X GET -i 'http://quizzes.websm.io/users/test/quizzes/24/responses/26' \
     -H"Authorization: Bearer $(cat ./tokens/eternal-admin.txt)"
